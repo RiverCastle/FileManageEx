01a03e3fb12fc99bd6ff5a1be222058c0baef419
RiverCastle <rkdtjd12@gmail.com>
2024-12-23 20:53:07 +0900

    first commit

---
+++ src/test/java/JesusDeciples/RankingQuiz/FileManageService/ApplicationTests.java
@@ -0,0 +1,13 @@
+package JesusDeciples.RankingQuiz.FileManageService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
